411a31cf808864200c2509b02a4aea42aee28528
user <user@users-macbook-pro-2.local>
2017-09-28 10:14:30 +0000

    首次提交

---
+++ weixin模仿/weixin模仿/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  weixin模仿
+//
+//  Created by user on 17/9/28.
+//  Copyright © 2017年 changandaxue. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ weixin模仿/weixin模仿/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  weixin模仿
+//
+//  Created by user on 17/9/28.
+//  Copyright © 2017年 changandaxue. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
